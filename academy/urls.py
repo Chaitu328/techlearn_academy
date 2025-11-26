816842ee633ff5f2e179e1afeaa81b54845fdd0b
chaitu328 <konakallachaitanya@gmail.com>
2025-11-26 15:55:36 +0530

    Add edit functionality for courses, trainers, and students; update templates and URLs

--- academy/urls.py
+++ academy/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('students/', views.student,name='student'),
     path('courses/add',views.add_course,name='add_course'),
     path('trainers/add',views.add_trainer,name='add_trainer'),
-    path('students/add',views.add_student,name='add_student')   
+    path('students/add',views.add_student,name='add_student'),
+    path('courses/edit/<int:id>/',views.edit_course,name='edit_course'),
+    path('trainers/edit/<int:id>/',views.edit_trainer,name='edit_trainer'),
+    path('students/edit/<int:id>/',views.edit_student,name='edit_student'),
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from academy.models import Course,Trainer,Student
 from .forms import CourseForm,TrainerForm,StudentForm
+from .models import Course,Trainer,Student
 # Create your views here.
 def course(request):
     courses = Course.objects.all()
@@ -72,3 +73,60 @@ def add_student(request):
     }
     return render(request,'add.html', context)
 
+
+def edit_course(request,id):
+    course = get_object_or_404(Course,id=id)
+    if request.method == 'POST':
+        form = CourseForm(request.POST,request.FILES,instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('course')
+        else:
+            print(form.errors)
+    else:
+        form = CourseForm(instance=course)
+    context = {
+        'form' : form,
+        'Title' : 'Course',
+        'action_url' : 'edit_course',
+        'obj': course,
+    }
+    return render(request,'edit.html',context)
+
+def edit_trainer(request,id):
+    trainer = get_object_or_404(Trainer,id=id)
+    if request.method == 'POST':
+        form = TrainerForm(request.POST,request.FILES,instance=trainer)
+        if form.is_valid():
+            form.save()
+            return redirect('trainer')
+        else:
+            print(form.errors)
+    else:
+        form = TrainerForm(instance=trainer)
+    context = {
+        'form' : form,
+        'Title' : 'Trainer',
+        'action_url' : 'edit_trainer',
+        'obj': trainer,
+    }
+    return render(request,'edit.html',context)
+
+def edit_student(request,id):
+    student = get_object_or_404(Student,id=id)
+    if request.method == 'POST':
+        form = StudentForm(request.POST,request.FILES,instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student')
+        else:
+            print(form.errors)
+    else:
+        form = StudentForm(instance=student)
+    context = {
+        'form' : form,
+        'Title' : 'Student',
+        'action_url' : 'edit_student',
+        'obj': student,
+    }
+    return render(request,'edit.html',context)
