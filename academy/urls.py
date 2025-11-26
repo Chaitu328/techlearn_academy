e24a36dc711fc3cc8b9643ce75abd7f386c6f6b2
chaitu328 <konakallachaitanya@gmail.com>
2025-11-26 13:18:21 +0530

    Implement forms for Course, Student, and Trainer; add functionality to create new entries

--- academy/urls.py
+++ academy/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('courses/', views.course,name='course'),
     path('trainers/', views.trainer,name='trainer'),
-    path('students/', views.student,name='student')
+    path('students/', views.student,name='student'),
+    path('courses/add',views.add_course,name='add_course'),
+    path('trainers/add',views.add_trainer,name='add_trainer'),
+    path('students/add',views.add_student,name='add_student')   
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from academy.models import Course,Trainer,Student
+from .forms import CourseForm,TrainerForm,StudentForm
 # Create your views here.
 def course(request):
     courses = Course.objects.all()
@@ -21,3 +22,53 @@ def student(request):
         'students' : students,
     }
     return render(request, 'student.html',context)
+
+
+def add_course(request):
+    if request.method == 'POST':
+        form =CourseForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('course')
+        else:
+            print(form.errors)
+    form = CourseForm()
+    context = {
+        'form' : form,
+        'Title': "Course",
+        'action_url': 'add_course',
+    }
+    return render(request,'add.html',context)
+
+def add_trainer(request):
+    if request.method == 'POST':
+        form = TrainerForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('trainer')
+        else:
+            print(form.errors)
+    form = TrainerForm()
+    context = {
+        'form' : form,
+        'Title': 'Trainer',
+        'action_url': 'add_trainer',
+    }
+    return render(request,'add.html', context)
+
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('student')
+        else:
+            print(form.errors)
+    form = StudentForm()
+    context = {
+        'form' : form,
+        'Title': 'Student',
+        'action_url' : 'add_student',
+    }
+    return render(request,'add.html', context)
+
